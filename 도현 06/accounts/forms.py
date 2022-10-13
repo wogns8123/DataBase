b758be3e540d02a00b8aedc28f5ac0fe86faeec8
정재훈 <wogns8123@naver.com>
2022-10-13 09:23:37 +0900

    DataBase

--- 도현 06/accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
-class CustomUserCreationForm(UserCreationForm):
-
-    class Meta(UserCreationForm.Meta):
-        model = get_user_model()
-        fields = UserCreationForm.Meta.fields + ('email',)
-
